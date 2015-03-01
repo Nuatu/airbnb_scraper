44aad915fb735d347037b520a5d18fbd11d321ca
Nuatu <nuatut@gmail.com>
2015-03-01 13:59:33 -0800

    Scrape max number of pages and store in max_page variable

--- airbnb_scraper.rb
+++ airbnb_scraper.rb
@@ -8,6 +8,16 @@ url = "https://www.airbnb.com/s/Brooklyn--NY--United-States"
 # Nokogiri to parse/save page
 page = Nokogiri::HTML(open(url))
 
+# Scrape max number of pages and store in max_page variable
+page_numbers = []
+page.css("div.pagination ul li a[target]").each do |line|
+ page_numbers << line.text
+end
+
+max_page = page_numbers.max
+
+puts max_page
+
 # Store data in arrays
 names = []
 page.css('div.h5.listing-name').each do |line|
@@ -24,11 +34,11 @@ page.css('div.text-muted.listing-location.text-truncate').each do |line|
   details << line.text.strip.split(/ · /)
 end
 
-# Write data to CSV file
-CSV.open("airbnb_listings.csv", "w") do |file|
-  file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
+# # Write data to CSV file
+# CSV.open("airbnb_listings.csv", "w") do |file|
+#   file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
 
-  names.length.times do |i|
-    file << [names[i], prices[i], details[i][0], details[i][1], details[i][2]]
-  end
-end
+#   names.length.times do |i|
+#     file << [names[i], prices[i], details[i][0], details[i][1], details[i][2]]
+#   end
+# end
