d89e62d3ddbc6adbedd7a5692e01a8dc53b43e4c
Nuatu <nuatut@gmail.com>
2015-03-01 14:59:17 -0800

    Scrape each page by looping max_page times, and save results in csv

--- airbnb_scraper.rb
+++ airbnb_scraper.rb
@@ -5,40 +5,49 @@ require 'csv'
 # URL to be scraped
 url = "https://www.airbnb.com/s/Brooklyn--NY--United-States"
 
-# Nokogiri to parse/save page
+# Parse page with Nokogiri
 page = Nokogiri::HTML(open(url))
 
-# Scrape max number of pages and store in max_page variable
+# Scrape the max number of pages and store in max_page variable
 page_numbers = []
 page.css("div.pagination ul li a[target]").each do |line|
  page_numbers << line.text
 end
 
-max_page = page_numbers.max
+max_page = page_numbers.max.to_i
 
-puts max_page
-
-# Store data in arrays
-names = []
-page.css('div.h5.listing-name').each do |line|
-  names << line.text.strip
-end
-
-prices = []
-page.css('span.h3.price-amount').each do |line|
-  prices << line.text
-end
-
+# Initialize empty arrays
+name = []
+price = []
 details = []
-page.css('div.text-muted.listing-location.text-truncate').each do |line|
-  details << line.text.strip.split(/ · /)
+
+# Loop once for each page of search results
+max_page.times do |i|
+
+  # Open search results page
+  url = "https://www.airbnb.com/s/Brooklyn--NY--United-States?page=#{i+1}"
+  page = Nokogiri::HTML(open(url))
+
+  # Store data in arrays
+  page.css('div.h5.listing-name').each do |line|
+    name << line.text.strip
+  end
+
+  page.css('span.h3.price-amount').each do |line|
+    price << line.text
+  end
+
+  page.css('div.text-muted.listing-location.text-truncate').each do |line|
+    details << line.text.strip.split(/ · /)
+  end
+
 end
 
-# # Write data to CSV file
-# CSV.open("airbnb_listings.csv", "w") do |file|
-#   file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
+# Write data to CSV file
+CSV.open("airbnb_listings.csv", "w") do |file|
+  file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
 
-#   names.length.times do |i|
-#     file << [names[i], prices[i], details[i][0], details[i][1], details[i][2]]
-#   end
-# end
+  name.length.times do |i|
+    file << [name[i], price[i], details[i][0], details[i][1], details[i][2]]
+  end
+end
